aaf6ae96d3c30eb022d83d523107fc55c481f7ee
João P M Morais <paulomedeiros5000@gmail.com>
2023-02-07 14:09:57 -0300

    completion of initial settings

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/.husky/", "/public/"],
+};
---
+++ src/App.spec.ts
@@ -0,0 +1,9 @@
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+it("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+export {};
